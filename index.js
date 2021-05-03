e34876cbb7b502770bb86df85d5225478c388bcf
Ali Kabalan <ali.kabalan.1994@gmail.com>
2021-05-03 09:26:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,12 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+
+
+superbowlWin = (record) => {
+  let result = record.find( record.result => record.result === "W" )
+  
+}
+
+// return !!result ? result.year : undefined
